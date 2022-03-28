84fcbf2fdc2b71c85cd447bfeb587a988e2eb9fd
kaderabdul <kaderabdul7980@gmail.com>
2022-03-29 01:55:12 +0600

    now can delete student data

--- backend/app/Http/Controllers/API/StudentController.php
+++ backend/app/Http/Controllers/API/StudentController.php
@@ -68,5 +68,25 @@ class StudentController extends Controller
 
     }
 
+    public function destroy($id)
+    {
+        $student = Student::find($id);
+        if($student)
+        {
+            $student->delete();
+            return response()->json([
+                'status'=> 200,
+                'message'=>'Student Deleted Successfully',
+            ]);
+        }
+        else
+        {
+            return response()->json([
+                'status'=> 404,
+                'message' => 'No Student ID Found',
+            ]);
+        }
+    }
+
 
 }
